7685d893b29f2dd8dc1da2c3e2db8350cf521181
zuyo7852 <leejuyeon7852@gmail.com>
2025-07-20 01:31:26 +0900

    setting

---
+++ src/main/java/com/ll/ch03_10/Ch0310Application.java
@@ -0,0 +1,13 @@
+package com.ll.ch03_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch0310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch0310Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/ch03_10/Ch0310ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.ch03_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch0310ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
