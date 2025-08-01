2d2be69b627d21188c70e046f534e9403b84d3c8
zuyo7852 <leejuyeon7852@gmail.com>
2025-08-01 17:25:45 +0900

    ch08-15

--- src/main/java/com/ll/ch03_10/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/ch03_10/domain/member/member/controller/ApiV1MemberController.java
@@ -2,9 +2,9 @@ package com.ll.ch03_10.domain.member.member.controller;
 
 import com.ll.ch03_10.domain.member.member.entity.Member;
 import com.ll.ch03_10.domain.member.member.service.MemberService;
-import com.ll.ch03_10.global.exceptions.GlobalException;
 import com.ll.ch03_10.global.rsData.RsData;
-import com.ll.ch03_10.standard.dto.util.Ut;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -24,35 +24,19 @@ public class ApiV1MemberController {
     @AllArgsConstructor
     @Getter
     private static class MemberJoinBody {
+        @NotBlank
         private String username;
+        @NotBlank
         private String password;
+        @NotBlank
         private String nickname;
     }
 
     @PostMapping("")
     public RsData<Member> join(
-        @RequestBody MemberJoinBody reqBody
+        @RequestBody @Valid MemberJoinBody reqBody
     ){
-        if (Ut.str.isBlank(reqBody.username)){
-            throw new GlobalException("400-1", "아이디를 입력해주세요");
-        }
-        if (Ut.str.isBlank(reqBody.password)){
-            throw new GlobalException("400-1", "비밀번호를 입력해주세요");
-        }
-        if (Ut.str.isBlank(reqBody.nickname)){
-            throw new GlobalException("400-1", "닉네임을 입력해주세요");
-        }
-        try{
-            return memberService.join(reqBody.username, reqBody.password, reqBody.nickname);
-        }
-        catch(GlobalException e){
-            if(e.getRsData().getResultCode().equals("400-1")){
-                log.debug("이미 존재하는 아이디입니다.");
-            }
-            return RsData.of("400-A", "이미 존재하는 아이디입니다.", Member.builder().build());
-        }
-
-
+        return memberService.join(reqBody.username, reqBody.password, reqBody.nickname);
     }
 
 }
--- src/main/java/com/ll/ch03_10/global/exceptions/exceptionHandlers/GlobalExceptionHandler.java
+++ src/main/java/com/ll/ch03_10/global/exceptions/exceptionHandlers/GlobalExceptionHandler.java
@@ -1,27 +1,61 @@
 package com.ll.ch03_10.global.exceptions.exceptionHandlers;
 
 import com.ll.ch03_10.global.exceptions.GlobalException;
+import com.ll.ch03_10.global.rq.Rq;
 import com.ll.ch03_10.global.rsData.RsData;
 import com.ll.ch03_10.standard.dto.Empty;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @ControllerAdvice
 @RequiredArgsConstructor
 @Slf4j
 public class GlobalExceptionHandler {
-    @ExceptionHandler(GlobalException.class) //더 구체적인것
-    @ResponseBody
-    public ResponseEntity<String> handleException(GlobalException ex) {
-        RsData<Empty> rsData = ex.getRsData();
+    private final Rq rq;
 
-        return ResponseEntity
-                .status(rsData.getStatusCode())
-                .body(rsData.getMsg());
+    public ResponseEntity<Object> handleApiException(Exception ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("resultCode", "500-1");
+        body.put("statusCode", 500);
+        body.put("msg", ex.getLocalizedMessage());
+
+        LinkedHashMap<String, Object> data = new LinkedHashMap<>();
+        body.put("data", data);
+
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        ex.printStackTrace(pw);
+        data.put("trace", sw.toString().replace("\t", "    ").split("\\r\\n"));
+
+        String path = rq.getCurrentUrlPath();
+        data.put("path", path);
+
+        body.put("success", false);
+        body.put("fail", true);
+
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(GlobalException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST) // 참고로 이 코드의 역할은 error 내용의 스키마를 타입스크립트화 하는데 있다.
+    @RequestMapping(produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<RsData<Empty>> handle(GlobalException ex) {
+        HttpStatus status = HttpStatus.valueOf(ex.getRsData().getStatusCode());
+        rq.setStatusCode(ex.getRsData().getStatusCode());
+
+        return new ResponseEntity<>(ex.getRsData(), status);
+    }
     }
 
 
