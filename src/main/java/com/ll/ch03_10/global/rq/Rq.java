6b53835c6dd963cd86038c80b96cd6cb0a679d87
zuyo7852 <leejuyeon7852@gmail.com>
2025-08-31 18:57:41 +0900

    #3 인증정보 체크의 비효율 제거

--- src/main/java/com/ll/ch03_10/global/rq/Rq.java
+++ src/main/java/com/ll/ch03_10/global/rq/Rq.java
@@ -2,12 +2,11 @@ package com.ll.ch03_10.global.rq;
 
 import com.ll.ch03_10.domain.member.member.entity.Member;
 import com.ll.ch03_10.domain.member.member.service.MemberService;
-import com.ll.ch03_10.global.exceptions.GlobalException;
-import com.ll.ch03_10.standard.util.Ut;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
@@ -25,31 +24,11 @@ public class Rq {
     public Member getMember(){
         if (member != null) return member;
 
-        String actorUsername = getCookieValue("actorUsername", null);
-        String actorPassword = getCookieValue("actorPassword", null);
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
 
-        if(actorUsername == null || actorPassword == null){
-            String authorization = req.getHeader("Authorization");
-            if( authorization != null ){
-                authorization = authorization.substring("bearer ".length());
-                String[] authorizationBits = authorization.split(" ", 2);//띄어쓰기 기준으로
-                actorUsername = authorizationBits[0];
-                actorPassword = authorizationBits.length == 2 ? authorizationBits[1] : null;
-            }
-        }
+        member = memberService.findByUsername(name).get();
 
-        if(Ut.str.isBlank(actorUsername)) throw new GlobalException("401-1", "인증정보(아이디)를 입력해주세요.");
-        if(Ut.str.isBlank(actorPassword)) throw new GlobalException("401-1", "인증정보(비밀번호)를 입력해주세요.");
-
-        Member loginedMember = memberService.findByUsername(actorUsername)
-                .orElseThrow(() -> new GlobalException("401-2", "해당회원이 존재하지 않습니다."));
-
-        if(!memberService.matchPassword(actorPassword, loginedMember.getPassword()))
-            throw new GlobalException("403-3", "해당 비밀번호가 일치하지 않습니다.");
-
-        member = loginedMember;
-
-        return loginedMember;
+        return member;
     }
 
     public String getCurrentUrlPath() {
