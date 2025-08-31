b01e8cce73ac24db2eb990338aaac3111c8369ed
zuyo7852 <leejuyeon7852@gmail.com>
2025-08-31 18:17:04 +0900

    #3 CustomAuthenticationFilter 추가

--- src/main/java/com/ll/ch03_10/global/security/SecurityConfig.java
+++ src/main/java/com/ll/ch03_10/global/security/SecurityConfig.java
@@ -1,12 +1,20 @@
 package com.ll.ch03_10.global.security;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
+@Slf4j
 public class SecurityConfig {
+    private final CustomAuthenticationFilter customAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -27,7 +35,8 @@ public class SecurityConfig {
                 ) //REST API에서는 사용하지 않는다
                 .formLogin(
                         formLogin -> formLogin.permitAll()
-                );
+                )
+                .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
