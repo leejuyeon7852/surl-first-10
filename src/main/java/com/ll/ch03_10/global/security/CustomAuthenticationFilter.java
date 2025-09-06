fbbbc2f96c018492660727adef2e9c259a4908ab
zuyo7852 <leejuyeon7852@gmail.com>
2025-09-06 15:17:02 +0900

    #4 JWT를 사용하여 인증

--- src/main/java/com/ll/ch03_10/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/ll/ch03_10/global/security/CustomAuthenticationFilter.java
@@ -1,6 +1,6 @@
 package com.ll.ch03_10.global.security;
 
-import com.ll.ch03_10.domain.member.member.entity.Member;
+import com.ll.ch03_10.domain.auth.auth.service.AuthTokenService;
 import com.ll.ch03_10.domain.member.member.service.MemberService;
 import com.ll.ch03_10.global.rq.Rq;
 import com.ll.ch03_10.standard.util.Ut;
@@ -17,38 +17,42 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.util.List;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationFilter extends OncePerRequestFilter {
     private final MemberService memberService;
+    private final AuthTokenService authTokenService;
     private final Rq rq;
 
     @Override
     @SneakyThrows
     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse resp, FilterChain filterChain) {
-        String apiKey = rq.getCookieValue("apiKey", null);
+        String accessToken = rq.getCookieValue("accessToken", null);
 
-        if(apiKey == null){
+        if(accessToken == null){
             String authorization = req.getHeader("Authorization");
             if( authorization != null ){
-                apiKey = authorization.substring("bearer ".length());
+                accessToken = authorization.substring("bearer ".length());
             }
         }
 
-        if(Ut.str.isBlank(apiKey)){
+        if(Ut.str.isBlank(accessToken)){
             filterChain.doFilter(req, resp);
             return;
         }
 
-        Member loginedMember = memberService.findByApiKey(apiKey).orElseThrow(null);
-        if(loginedMember == null){
+        if(!authTokenService.validateToken(accessToken)){
             filterChain.doFilter(req, resp);
             return;
         }
+
+        Map<String, Object> accessTokenData = authTokenService.getDataFrom(accessToken);
+        long id = (int) accessTokenData.get("id");
 
         //인증..?
-        User user = new User(loginedMember.getId()+"", "", List.of());
+        User user = new User(id + "", "", List.of());
         Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
