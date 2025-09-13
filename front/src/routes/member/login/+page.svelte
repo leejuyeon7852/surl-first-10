21838383cb1196e9c0d5a98b0951095239b6b3b9
zuyo7852 <leejuyeon7852@gmail.com>
2025-09-13 16:37:23 +0900

    #7 로그인 페이지

---
+++ front/src/routes/member/login/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	async function submitLoginForm(this: HTMLFormElement) {
+		const form: HTMLFormElement = this;
+
+		form.username.value = form.username.value.trim();
+
+		if (form.username.value.length === 0) {
+			alert('아이디를 입력해주세요.');
+			form.username.focus();
+			return;
+		}
+
+		form.password.value = form.password.value.trim();
+
+		if (form.password.value.length === 0) {
+			alert('비밀번호를 입력해주세요.');
+			form.password.focus();
+			return;
+		}
+
+		const rs = await fetch('http://localhost:8070/api/v1/members/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				username: form.username.value,
+				password: form.password.value
+			})
+		}).then((res) => res.json());
+		console.log(rs);
+	}
+</script>
+
+<form on:submit|preventDefault={submitLoginForm}>
+	<div>
+		<label>
+			<span>username</span>
+			<input type="text" name="username" placeholder="아이디" />
+		</label>
+	</div>
+	<div>
+		<label>
+			<span>비밀번호</span>
+			<input type="password" name="password" placeholder="비밀번호" />
+		</label>
+	</div>
+	<div>
+		<label>
+			<span>회원가입</span>
+			<input type="submit" value="회원가입" />
+		</label>
+	</div>
+</form>
