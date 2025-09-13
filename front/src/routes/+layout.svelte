c51874ebcf9e8d424fdda5e2a612e7d984c099d8
zuyo7852 <leejuyeon7852@gmail.com>
2025-09-13 16:08:10 +0900

    #7 레이아웃

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,5 +1,14 @@
-<nav>
-	<a href="/">메인</a>
-	<a href="/about">설명</a>
-	<a href="/home/links">링크들</a>
-</nav>
+<header>
+	<nav>
+		<a href="/">메인</a>
+		<a href="/about">설명</a>
+		<a href="/home/links">링크들</a>
+	</nav>
+</header>
+
+<slot></slot>
+<footer>
+	<nav>
+		<a href="/home/links">링크들</a>
+	</nav>
+</footer>
--- front/src/routes/home/links/+page.svelte
+++ front/src/routes/home/links/+page.svelte
@@ -1,5 +1,1 @@
-<a href="/">메인</a>
-<a href="/about">설명</a>
-<a href="/home/links">링크들</a>
-
 <h1>링크들</h1>
