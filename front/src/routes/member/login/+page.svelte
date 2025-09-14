2b3408b6b7ace24aeb384bf98e83bd663328f228
zuyo7852 <leejuyeon7852@gmail.com>
2025-09-14 14:22:02 +0900

    #7 프론트엔드 운영환경 분리

--- front/src/routes/member/login/+page.svelte
+++ front/src/routes/member/login/+page.svelte
@@ -18,7 +18,7 @@
 			return;
 		}
 
-		const rs = await fetch('http://localhost:8070/api/v1/members/login', {
+		const rs = await fetch(`${import.meta.env.VITE_CORE_API_BASE_URL}/api/v1/members/login`, {
 			method: 'POST',
 			credentials: 'include',
 			headers: {
